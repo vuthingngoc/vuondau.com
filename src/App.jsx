a51caa76f09e0b938a2b1e93e358e27fb5c9a527
vuthingngoc <63228228+vuthingngoc@users.noreply.github.com>
2021-09-30 19:20:29 +0700

    update master

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Homepage from './pages/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" render={() => <Homepage />} />
-      <Redirect to="/" />
+      <Switch>
+        <Route exact path="/" render={() => <Homepage />} />
+        <Redirect to="/" />
+      </Switch>
     </BrowserRouter>
   );
 }
