d693864ff0f3384c622ce14c0913da636bf21793
vuthingngoc <63228228+vuthingngoc@users.noreply.github.com>
2021-10-05 22:15:47 +0700

    update homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ColorNavbar from 'components/Navbars/ColorNavbar';
-import IndexHeader from 'components/Headers/IndexHeader';
 import HeaderSlider from './components/HeaderSlider';
 import BodyProduction from './components/BodyProduct';
 import FooterEcommerce from 'components/Footers/FooterEcommerce';
+import SettingsHeader from 'components/Headers/SettingsHeader';
 
 export default function Homepage() {
   return (
     <>
       <ColorNavbar />
-      <IndexHeader />
+      <SettingsHeader />
       <HeaderSlider />
       <BodyProduction />
       <FooterEcommerce />
